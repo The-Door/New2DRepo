32a9db2d1b9f47c4a0a60f73113b7f6f13aa48ce
The-Door <Spionagenten@gmail.com>
2023-10-24 20:55:27 +0200

    ljus

--- New2D/Assets/LightSwitch.cs
+++ New2D/Assets/LightSwitch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VSCode : MonoBehaviour
+public class LightSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
